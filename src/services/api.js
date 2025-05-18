d697d70299fe91d4c7c91dab6472892a76e82602
3pservicepartner <3pservicepartner2@gmail.com>
2025-05-18 11:28:15 -0300

    commit 18052025-1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:8085', //'http://18.230.175.8:8085'  localhost
+    baseURL: 'http://56.125.31.77:8085', //'http://56.125.31.77:8085'  localhost
     withCredentials: true
 })
 
